8271842dc552e28da68f9ec85c6519d62d060b99
dkalabukhov <daniil.kalabukhov@gmail.com>
2025-04-01 18:07:15 +0300

    Solutions for module 2, fix for module 1/task 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
-let calculator = {
-  // ваш код
+const calculator = {
+  read(firstNum, secondNum) {
+    this.firstNum = firstNum;
+    this.secondNum = secondNum;
+  },
+
+  sum() {
+    return this.firstNum + this.secondNum;
+  },
+
+  mul() {
+    return this.firstNum * this.secondNum;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ

195d2fb9f5c16a7259694e48c434403264ab8e01
dkalabukhov <daniil.kalabukhov@gmail.com>
2025-04-12 17:08:23 +0300

    Solutions for module-5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
-  // ваш код...
+  function updateCarousel() {
+    elements.arrowLeft.style.display = state.currentSlideIndex === 0 ? 'none' : '';
+    elements.arrowRight.style.display = state.currentSlideIndex === slidesCount - 1 ? 'none' : '';
+
+    const offset = -state.currentSlideIndex * slideWidth;
+    elements.carouselInner.style.transform = `translateX(${offset}px)`;
+  }
+
+  const elements = {
+    arrowRight: document.querySelector('.carousel__arrow_right'),
+    arrowLeft: document.querySelector('.carousel__arrow_left'),
+    carouselInner: document.querySelector('.carousel__inner'),
+  };
+
+  const state = {
+    currentSlideIndex: 0,
+  };
+
+  const slidesCount = elements.carouselInner.children.length;
+  const slideWidth = elements.carouselInner.offsetWidth;
+
+  elements.arrowRight.addEventListener('click', () => {
+    if (state.currentSlideIndex === slidesCount - 1) {
+      return;
+    }
+
+    state.currentSlideIndex += 1;
+    updateCarousel();
+  });
+
+  elements.arrowLeft.addEventListener('click', () => {
+    if (state.currentSlideIndex === 0) {
+      return;
+    }
+
+    state.currentSlideIndex -= 1;
+    updateCarousel();
+  })
+
+  updateCarousel();
 }
